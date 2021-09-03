56848969fb4fdee93c0615a4b1bea468b06d7ff3
陈国庆 <cgq786153492@gmail.com>
2021-09-03 17:26:30 +0800

    fix

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'dist')))
   .set('views', path.join(__dirname, 'dist'))
-  .get('/', (req, res) => res.render('index.js'))
+  .get('/', (req, res) => res.send('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
