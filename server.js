11af0e445b6f7af4fdc1cf711b7631a93e4ae428
gqchen-gcs <cgq786153492@gmail.com>
2021-09-06 01:56:08 +0000

    fix

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'dist')))
-  .set('views', path.join(__dirname, 'dist'))
-  .get('/', (req, res) => res.render('index.js'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
